8cb8d8b7c1626697592ed638673e479266a4c00a
Max Nicholson <max.nicholson@calmid.com>
2021-06-10 16:57:53 +0100

    test: setup jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    // Indicates whether the coverage information should be collected while executing the test
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    preset: 'ts-jest',
+}
