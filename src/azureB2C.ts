f5b879ac337a9221d9ce475ca917e2af5277fab1
Max Nicholson <max.nicholson@kidsloop.live>
2021-11-30 12:07:43 +0000

    Merged in feature/IAM-478-update-b2c-configs-to-replace-te (pull request #17)
    feat: update B2C config to use custom domain
    
    * feat: update B2C config to use custom domain
    
    
    Approved-by: Oleksandra.Holubitska
    Approved-by: David Nimmo

--- src/azureB2C.ts
+++ src/azureB2C.ts
@@ -6,13 +6,10 @@ import {
     ITokenPayload,
 } from 'passport-azure-ad'
 
-import { 
-    AzureB2CTokenPayload,
-    IdToken
-} from './types/token'
+import { AzureB2CTokenPayload, IdToken } from './types/token'
 
 const credentials = {
-    tenantName: process.env.AZURE_B2C_TENANT_NAME,
+    tenantID: process.env.AZURE_B2C_TENANT_ID,
     clientID: process.env.AZURE_B2C_CLIENT_ID || '',
 }
 const policies = {
@@ -20,6 +17,7 @@ const policies = {
 }
 const metadata = {
     authority: process.env.AZURE_B2C_AUTHORITY,
+    domain: process.env.AZURE_B2C_DOMAIN,
     discovery: '.well-known/openid-configuration',
     version: 'v2.0',
 }
@@ -30,9 +28,8 @@ const settings = {
     loggingLevel: 'warn',
 }
 
-
 const options: IBearerStrategyOptionWithRequest = {
-    identityMetadata: `https://${credentials.tenantName}.b2clogin.com/${credentials.tenantName}.onmicrosoft.com/${policies.policyName}/${metadata.version}/${metadata.discovery}`,
+    identityMetadata: `https://${metadata.domain}/${credentials.tenantID}/${policies.policyName}/${metadata.version}/${metadata.discovery}`,
     clientID: credentials.clientID,
     audience: credentials.clientID,
     policyName: policies.policyName,
@@ -50,32 +47,34 @@ const bearerStrategy = () =>
             done(null, {}, token)
         }
     )
-    
+
 if (process.env.AZURE_B2C_ENABLED === 'true') {
     passport.initialize()
     passport.use(bearerStrategy())
 }
 
-export async function transferAzureB2CToken(
-    req: Request
-): Promise<IdToken> {
+export async function transferAzureB2CToken(req: Request): Promise<IdToken> {
     return new Promise<IdToken>((resolve, reject) => {
         passport.authenticate(
             'oauth-bearer',
             { session: false },
-            (err: Error | null, user: boolean | never, info: AzureB2CTokenPayload) => {
+            (
+                err: Error | null,
+                user: boolean | never,
+                info: AzureB2CTokenPayload
+            ) => {
                 if (err) {
                     return reject(err)
                 }
                 if (!user) {
                     return reject({ message: 'Invalid token' })
                 }
-                if(!info.emails || info.emails.length === 0) {
+                if (!info.emails || info.emails.length === 0) {
                     return reject({ message: 'missing emails claim' })
                 }
                 const idToken = {
                     name: info.name,
-                    email: info.emails[0]
+                    email: info.emails[0],
                 }
                 return resolve(idToken)
             }
